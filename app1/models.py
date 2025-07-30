659b247003a12e000d1fc4483d996ec75748e293
Anmol-Nishad06 <anmolnishad20june@gmail.com>
2025-07-31 00:13:13 +0530

    create tech model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+Tech_Typyes = [
+    ('AI', 'Artificial Intelligence'), 
+    ('ML', 'Machine Learning'),
+    ('DL', 'Deep Learning'),
+    ('NLP', 'Natural Language Processing'),
+    ('CV', 'Computer Vision'),
+]
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=3, choices=Tech_Typyes, default='AI')
+
+    def __str__(self):
+        return self.name
--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,8 +127,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Directory for static files
+    os.path.join(BASE_DIR / "static")  # Directory for static files
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media') # Directory for media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
